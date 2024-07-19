7b4f642987c3ee82d43efb5a0a9e2b4eb24e7fd7
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-07-19 20:58:28 +0500

    in app.js game start demo eventlistner is created

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let msg = document.querySelector(".msg");
+let gameStart = false;
+addEventListener("keypress", function (evt) {
+  if (gameStart == false) {
+    console.log("Event Occured");
+    gameStart = true;
+  }
+});
+addEventListener("click", function (evt) {
+  if (gameStart == false) {
+    console.log("Event Occured");
+    gameStart = true;
+  }
+});
