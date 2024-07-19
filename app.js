ad9b644ce6795585958725fb3360703ae23a5522
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-07-19 22:55:28 +0500

    in app.js a console stat is removed & a box taken from randIdx and added class flash to it and then removed using setTimeout and the box id value is stored in a sep var to push in compInput array and then pushed

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ function gameStarted() {
   level++;
   msg.innerText = `Level ${level}`;
   let randomIdx = Math.floor(Math.random() * 4);
+  let box = boxes[randomIdx];
+  box.classList.add("flash");
+  setTimeout(() => {
+    box.classList.remove("flash");
+  }, 300);
+  let compIn = box.getAttribute("id");
+  compInput.push(compIn);
 }
 addEventListener("keypress", function (evt) {
   if (gameStart == false) {
@@ -18,7 +25,6 @@ addEventListener("keypress", function (evt) {
 });
 addEventListener("click", function (evt) {
   if (gameStart == false) {
-    console.log("Event Occured");
     gameStart = true;
     gameStarted();
   }
