3013ee403b4878443d7520f0cc7a5206149f6f39
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-07-19 21:11:35 +0500

    in app.js msg updater for startgame is created

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 let msg = document.querySelector(".msg");
 let gameStart = false;
+let level = 0;
+function gameStarted() {
+  level++;
+  msg.innerText = `Level ${level}`;
+}
 addEventListener("keypress", function (evt) {
   if (gameStart == false) {
     console.log("Event Occured");
     gameStart = true;
+    gameStarted();
   }
 });
 addEventListener("click", function (evt) {
   if (gameStart == false) {
     console.log("Event Occured");
     gameStart = true;
+    gameStarted();
   }
 });
