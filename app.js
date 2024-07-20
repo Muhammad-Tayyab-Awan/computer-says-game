0fc671a7fe5023a53570fdf6b976f5e3a5113b26
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-07-20 16:21:27 +0500

    in app.js node for resetButton is created,a new variable to store userTurn is created to handle events,a new vriable userInCount to store userinputcount for game,a node is created  for body to change color of body to red in case of gameover,a function named gameStarted() is modified to start game first time and made asynced,a function randBox()is created,two eventListner for starting game is modified,a new function gameOver() is created,a new function flashBox(box) is created,a new function compArray(arr1,arr2) is created,a new function leveUp() is created,a new function startingGame(box) is created,also added event listener for startingGame,a new function resetGame() is created,and also event listener is added for reseting game

--- app.js
+++ app.js
@@ -4,27 +4,113 @@ let msg = document.querySelector(".msg");
 let gameStart = false;
 let level = 0;
 let boxes = document.querySelectorAll(".roundBox");
-function gameStarted() {
+let resetBtn = document.querySelector("#reset");
+let userTurn = false;
+let userInCount = 0;
+let bodyColor = document.querySelector("#body");
+async function gameStarted() {
+  resetBtn.classList.remove("disable");
   level++;
-  msg.innerText = `Level ${level}`;
-  let randomIdx = Math.floor(Math.random() * 4);
-  let box = boxes[randomIdx];
-  box.classList.add("flash");
-  setTimeout(() => {
-    box.classList.remove("flash");
-  }, 300);
+  msg.innerText = `Level : ${level}`;
+  let box = randBox();
+  await flashBox(box);
   let compIn = box.getAttribute("id");
   compInput.push(compIn);
+}
+function randBox() {
+  let randomIdx = Math.floor(Math.random() * 4);
+  let box = boxes[randomIdx];
+  return box;
 }
 addEventListener("keypress", function (evt) {
   if (gameStart == false) {
     gameStart = true;
+    userTurn = true;
     gameStarted();
   }
 });
 addEventListener("click", function (evt) {
   if (gameStart == false) {
     gameStart = true;
+    userTurn = true;
     gameStarted();
   }
 });
+function gameOver() {
+  bodyColor.classList.add("red");
+  msg.innerText = `GAME OVER! Your Score : ${level}`;
+}
+function flashBox(box) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      box.classList.add("flash");
+    }, 200);
+    setTimeout(() => {
+      box.classList.remove("flash");
+      resolve();
+    }, 1000);
+  });
+}
+function compArray(arr1, arr2) {
+  let length, valuesEqual;
+  if (arr1.length == arr2.length) {
+    length = arr1.length;
+    valuesEqual = 0;
+    for (let i = 0; i < length; i++) {
+      if (arr1[i] === arr2[i]) {
+        valuesEqual++;
+      }
+    }
+  }
+  return valuesEqual === length;
+}
+async function levelUp() {
+  userTurn = false;
+  userInCount = 0;
+  level++;
+  msg.innerText = `Level : ${level}`;
+  let box = randBox();
+  await flashBox(box);
+  let compIn = box.getAttribute("id");
+  compInput.push(compIn);
+  userTurn = true;
+}
+async function startingGame(box) {
+  await flashBox(box);
+  let userIn = box.getAttribute("id");
+  userInput.push(userIn);
+  userInCount++;
+  let areEqual = compArray(compInput, userInput);
+  if (areEqual === true && userInCount === compInput.length) {
+    userInput = [];
+    levelUp();
+  } else if (
+    userInCount <= compInput.length &&
+    compArray(userInput, compInput.slice(0, userInput.length)) !== true
+  ) {
+    gameOver();
+  } else if (userInCount > compInput.length) {
+    gameOver();
+  }
+}
+for (let i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener("click", (evt) => {
+    if (gameStart === true && userTurn === true) {
+      startingGame(boxes[i]);
+    }
+  });
+}
+function resetGame() {
+  if (gameStart === true) {
+    gameStart = false;
+    userTurn = false;
+    userInCount = 0;
+    msg.innerText = "Press Any Key or Tap on Screen Any Where To Start";
+    compInput = [];
+    userInput = [];
+    level = 0;
+    bodyColor.classList.remove("red");
+    resetBtn.classList.add("disable");
+  }
+}
+resetBtn.addEventListener("dblclick", resetGame);
